e0396d4ee4b70fb5cf7daa86145587bead7705c9
Clemens Horn <clemens.horn@mni.thm.de>
2024-01-24 14:51:08 +0100

    chore(): setup rust-analyzer

--- flake.nix
+++ flake.nix
@@ -2,31 +2,55 @@
   description = "draken development shell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs = {
+       url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+        flake-utils = {
+          follows = "flake-utils";
+        };
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-      in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
-            rust-bin.stable.latest.default
-            nodejs_20
-            corepack_20
-          ];
+        rustToolchain = (pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+          extensions = ["rust-src" "cargo" "rustc" "clippy"];
         };
-      }
+        nativeBuildInputs = with pkgs; [rustToolchain pkg-config];
+        buildInputs = with pkgs; [
+          openssl
+          rust-analyzer
+          nodejs_20
+          corepack_20
+        ];
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            inherit buildInputs nativeBuildInputs;
+            RUST_BACKTRACE = 1;
+            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          };
+        }
     );
 }
-
