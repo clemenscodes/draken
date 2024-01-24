e07198f36e0f0b3b7d1d6f85a2f7bad3a4117d21
Clemens Horn <clemens.horn@mni.thm.de>
2024-01-25 00:51:05 +0100

    fix(gui): circular structs  in mvc

--- bin/gui/src/main.rs
+++ bin/gui/src/main.rs
@@ -1,6 +1,42 @@
-use std::process::exit;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+pub struct Model {}
+
+#[derive(Debug)]
+pub struct View {
+    pub controller: Rc<Controller>,
+}
+
+#[derive(Debug)]
+pub struct Controller {
+    view: Weak<View>,
+    model: Model,
+}
+
+impl Controller {
+    fn set_view(&mut self, view: Weak<View>) {
+        self.view = view;
+    }
+    fn set_model(&mut self, model: Model) {
+        self.model = model;
+    }
+}
+
+impl View {
+    fn set_controller(&mut self, controller: Rc<Controller>) {
+        self.controller = controller;
+    }
+}
 
 fn main() {
-    let code = view::run("engine.chess.draken");
-    exit(code)
+    let mut view = Rc::new_cyclic(|view| View {
+        controller: Rc::new(Controller {
+            view: view.clone(),
+            model: Model {},
+        }),
+    });
+    let mut controller = view.controller.clone();
+    println!("{controller:#?}");
+    println!("{view:#?}");
 }
