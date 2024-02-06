a987d1d02605eeaec1dff3af6808f8cb52ad9ab7
Clemens Horn <clemens.horn@mni.thm.de>
2024-02-06 12:55:23 +0100

    fix(ui): jest install error

--- apps/ui/jest.config.ts
+++ apps/ui/jest.config.ts
@@ -3,7 +3,6 @@ export default {
     displayName: "ui",
     preset: "../../jest.preset.js",
     transform: {
-        "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nx/react/plugins/jest",
         "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nx/next/babel"] }],
     },
     moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
